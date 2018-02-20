157c5add226f4db9bf73bbb174f23d65e0c1fb9a
Fernando Papito <fernando@qaninja.io>
2018-02-19 21:55:31 -0300

    first commit

---
+++ spec/caixa_spec.rb
@@ -0,0 +1,23 @@
+
+require_relative '../app/conta.rb'
+
+describe('Saque') do
+  it('saque no caixa') do
+    conta = Conta.new('Fernando')
+    conta.saldo = 1000
+    conta.saque(100)
+    expect(conta.saldo).to eql 900
+  end
+  it('valor superior') do
+    conta = Conta.new('Fernando')
+    conta.saldo = 500
+    conta.saque(600)
+    expect(conta.saldo).to eql 500
+  end
+  it('valor limite') do
+    conta = Conta.new('Fernando')
+    conta.saldo = 1000
+    conta.saque(701)
+    expect(conta.saldo).to eql 1000
+  end
+end
